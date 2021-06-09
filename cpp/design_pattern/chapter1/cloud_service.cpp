778bca2b01a85ba0eca67237bc19c0dc5a558ac4
1000rym <qianlin725@gamil.com>
2021-06-09 20:53:16 +0900

    modify the source code.

--- cpp/design_pattern/chapter1/cloud_service.cpp
+++ cpp/design_pattern/chapter1/cloud_service.cpp
@@ -1,25 +1,26 @@
 #include "cloud_service.h"
 
-void File::set_upload_size(int upload_size)
-{
-    this->uploaded_size = upload_size;
+void UploadableFile::set_upload_size(int upload_size){
+    uploaded_size = upload_size;
+}
+
+void CloudService::set_upload_file_size(UploadableFile& uploadable_file, int size){
+    uploadable_file.set_upload_size(size);
+}
+
+void CloudService::get_upload_status(UploadableFile& uploadable_file){
+    uploadable_file.print_uploaded_status();
 }
 
 void File::print_uploaded_status(){
-    cout << "The file's upload progress:" << to_string(uploaded_size*100/file_size)<<"%."<<endl;
-}
-
-void CloudService::set_upload_file_size(File& file, int size){
-    file.set_upload_size(size);
-}
-
-void CloudService::get_upload_status(File& file){
-    file.print_uploaded_status();
+    cout<< "File Name: " << file_name << endl; 
+    cout<< "File size: " << file_size << endl;
+    cout<< "Uploaded size: " << uploaded_size << endl; 
 }
 
 int main(){
-    File file_a("FileA", 100);
-    CloudService cloudService; 
+    File file_a("FileA", 24);
+    CloudService cloudService;
     cloudService.set_upload_file_size(file_a, 35);
     cloudService.get_upload_status(file_a);
 }
--- cpp/design_pattern/chapter1/cloud_service.h
+++ cpp/design_pattern/chapter1/cloud_service.h
@@ -1,21 +1,40 @@
 #include <iostream>
 using namespace std; 
 
+
+class UploadableFile{
+    protected: 
+        int uploaded_size;
+        string file_name; 
+        int file_size; 
+      
+    public: 
+        UploadableFile (string file_name, int size)
+            :file_name(file_name), file_size(size), uploaded_size(0){};
+
+        void set_upload_size(int upload_size);
+        virtual void print_uploaded_status() = 0;
+};
+
+//Here is the cloud service which can directly mange the server to up load the file.
+class CloudService{
+    public: 
+        void set_upload_file_size(UploadableFile& file, int size);
+        void get_upload_status(UploadableFile& file);
+};
+
+
 //First, there is a file class, the file could be upload to cloud service. 
-class File{
+class File : public UploadableFile{
     private: 
         string name;
         int file_size; 
-        int uploaded_size;  
     public: 
-        File(string name, int file_size): name(name), file_size(file_size), uploaded_size(0){};
-        void set_upload_size(int upload_size);
+        File(string name, int file_size): UploadableFile(name, file_size){};
+        //override function from the Uploadable File. 
         void print_uploaded_status();
 };
 
-//And then,  we want a music class, which can also upload to cloud service.
-class CloudService{
-    public: 
-        void set_upload_file_size(File& file, int size);
-        void get_upload_status(File& file);
-};
+
+
+
