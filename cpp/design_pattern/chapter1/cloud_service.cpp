7eb9a6e5f266acec766a43fd7b00e13ca77a6495
1000rym <qianlin725@gamil.com>
2021-06-11 18:22:40 +0900

    modify the code

--- cpp/design_pattern/chapter1/cloud_service.cpp
+++ cpp/design_pattern/chapter1/cloud_service.cpp
@@ -1,7 +1,7 @@
 #include "cloud_service.h"
 
 void UploadableFile::set_upload_size(int upload_size){
-    uploaded_size = upload_size;
+    this->uploaded_size = upload_size;
 }
 
 void CloudService::set_upload_file_size(UploadableFile& uploadable_file, int size){
@@ -9,18 +9,23 @@ void CloudService::set_upload_file_size(UploadableFile& uploadable_file, int siz
 }
 
 void CloudService::get_upload_status(UploadableFile& uploadable_file){
-    uploadable_file.print_uploaded_status();
+    uploadable_file.print_uploaded_status(); 
 }
 
 void File::print_uploaded_status(){
-    cout<< "File Name: " << file_name << endl; 
-    cout<< "File size: " << file_size << endl;
-    cout<< "Uploaded size: " << uploaded_size << endl; 
+    cout << "The file is uploaded about:"<<to_string(uploaded_size*100/size)<<endl;
+}
+
+void Music::print_uploaded_status(){
+    cout << "The album is uploaded about:"<<to_string(uploaded_size*100/album_size)<<endl;
 }
 
 int main(){
-    File file_a("FileA", 24);
+    File file_a("FileA", 100);
+    Music music_a("MusicA", 10);
     CloudService cloudService;
     cloudService.set_upload_file_size(file_a, 35);
     cloudService.get_upload_status(file_a);
+    cloudService.set_upload_file_size(music_a, 5);
+    cloudService.get_upload_status(music_a);
 }
--- cpp/design_pattern/chapter1/cloud_service.h
+++ cpp/design_pattern/chapter1/cloud_service.h
@@ -5,12 +5,9 @@ using namespace std;
 class UploadableFile{
     protected: 
         int uploaded_size;
-        string file_name; 
-        int file_size; 
       
     public: 
-        UploadableFile (string file_name, int size)
-            :file_name(file_name), file_size(size), uploaded_size(0){};
+        UploadableFile (int size) : uploaded_size(0){};
 
         void set_upload_size(int upload_size);
         virtual void print_uploaded_status() = 0;
@@ -28,13 +25,21 @@ class CloudService{
 class File : public UploadableFile{
     private: 
         string name;
-        int file_size; 
+        int size; 
     public: 
-        File(string name, int file_size): UploadableFile(name, file_size){};
+        File(string name, int size): name(name),size(size), 
+        UploadableFile(size){};
         //override function from the Uploadable File. 
         void print_uploaded_status();
 };
 
-
-
-
+//Second, we Created music.
+class Music : public UploadableFile{
+    private: 
+        string album_name; 
+        int album_size; 
+    public: 
+        Music(string album_name, int album_size): album_name(album_name), album_size(album_size), 
+        UploadableFile(album_size){};
+        void print_uploaded_status();
+};
