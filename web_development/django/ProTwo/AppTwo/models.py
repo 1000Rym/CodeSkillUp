621b22e6b95dc69f244f66e4ab85df2f2060df51
1000Rym <qianlin725@gmail.com>
2021-11-17 11:26:01 +0900

    modify the excersice2

--- web_development/django/ProTwo/AppTwo/models.py
+++ web_development/django/ProTwo/AppTwo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length = 32, unique = True)
-    last_name = models.CharField(max_length = 32, unique = False)
-    email = models.CharField(max_length = 64, unique = True)
+    first_name = models.CharField(max_length = 128)
+    last_name = models.CharField(max_length = 128)
+    email = models.EmailField(max_length = 128, unique = True)
 
     def __str__(self):
         return  self.first_name + " " + self.last_name\
