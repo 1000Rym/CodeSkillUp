2a6b8ab1559c9ac406ce345c786e0ff7de350fbf
1000Rym <qianlin725@gmail.com>
2022-01-07 17:20:51 +0900

    add quiz34

---
+++ coding_test/dynamic_programming/q34_arrange_military.py
@@ -0,0 +1,26 @@
+""" 군사배치하기 Page380
+"""
+        
+
+def solution(members):
+    """
+    f[i] = max(f[i], f[j]+1) if member[i]<member[j]
+    """
+    f = [1]*len(members)
+    
+    for i in range(1,len(members)):
+        for j in range(0, i):
+            if members[i]<members[j]:
+                f[i] = max(f[i], f[j]+1) 
+    
+    return len(members)-max(f)
+
+def main():
+    n = int(input())
+    
+    members = list(map(int, input().split()))
+        
+    print(solution(members))
+
+if __name__ == '__main__':
+    main()
