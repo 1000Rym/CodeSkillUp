3905421250d4eefdadc33dbd39365fb122712cf4
1000Rym <qianlin725@gmail.com>
2022-01-31 15:27:21 +0900

    add a new python quiz

--- coding_test/coding_test_with_python/shortest_path/q39_mars_exploration.py
+++ coding_test/coding_test_with_python/shortest_path/q39_mars_exploration.py
@@ -34,3 +34,72 @@
 19
 36
 """
+
+import heapq
+INF = int(1e9)
+
+def find_nearby_positions(pos, n):
+    """Find nearby pos"""
+    nearby_list = []
+    x, y = pos[0], pos[1]
+     
+    if  x + 1 < n :
+        nearby_list.append((x+1, y))
+    if  x-1 >=0:
+        nearby_list.append((x-1, y))
+    if  y +1 < n : 
+        nearby_list.append((x, y+1))
+    if  y -1 >= 0:
+        nearby_list.append((x, y-1))
+        
+    return nearby_list     
+         
+
+def solution(n, map_info):
+    """ Dijkstra Algorithm
+    To solove this problem, we use dijkstra algorithm.
+    - INF the All maps.
+    - Push the item to heapq which is smaller than current item.
+    - Finish until the q is empty.
+    
+    """
+    start = (0, 0)
+    graph_info = [[INF] * n for _ in range(n)]
+    q = []
+    
+    heapq.heappush(q, (map_info[start[0]][start[1]], start))
+    
+    while q:
+        rank, pos = heapq.heappop(q)
+        nearby_list= find_nearby_positions(pos, n)
+        
+        for x, y in nearby_list:
+            new_rank = rank + map_info[x][y]
+            
+            if new_rank < graph_info[x][y]: 
+                graph_info[x][y] = new_rank
+                heapq.heappush(q, (new_rank, (x, y)))
+    
+    return graph_info[n-1][n-1]
+          
+
+def main():
+    
+    case_count = int(input())
+    answers = []
+    
+    for _ in range(case_count):
+        n = int(input())
+        
+        map_info = []
+        for _ in range(n):
+            line_info = list(map(int, input().split()))
+            map_info.append(line_info)
+        answers.append(solution(n, map_info))
+        
+    for answer in answers: 
+        print(answer)
+    
+    
+if __name__ == '__main__':
+    main()
